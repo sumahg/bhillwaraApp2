cf1248933e6565cf06a8dd4d7c658f8423f39495
suma <sumahg.g@gmail.com>
2019-03-19 15:12:04 +0530

    second commit

---
+++ src/app/tab5/tab5.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab5',
+  templateUrl: './tab5.component.html',
+  styleUrls: ['./tab5.component.scss'],
+})
+export class Tab5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
